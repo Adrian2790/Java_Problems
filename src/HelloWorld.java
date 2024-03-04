dd3ad88ce6ae2697ccd5d007ed139fa0ef460db3
Adrian2790 <adrian.sofian99@gmail.com>
2024-03-04 22:39:19 +0200

    Commit2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Worldddd!");
+        System.out.println("Hello, Worldddddddd!");
     }
 }
